0b7593b78cd95548f203789611f9f6a6bc32a892
harshal4x <talk.harshal@gmail.com>
2026-01-30 15:51:07 +0530

    .env

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -58,9 +58,14 @@ export default function Signin({ setisSignin }) {
           className="w-full mb-6 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
 
-        <button onClick={SigninHandler} className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition">
-          Sign In
-        </button>
+        {
+          (loading) ? 
+              <h5>Loading...</h5> 
+            : 
+          <button onClick={SigninHandler} className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition">
+            Sign In
+          </button>
+        }
 
         <p className="text-sm text-center mt-4 text-gray-600">
           <NavLink to='/signup'>
