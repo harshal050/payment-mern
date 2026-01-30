aab3edd464366f15b05fed9c8e4f6e347cf8e65e
harshal4x <talk.harshal@gmail.com>
2026-01-30 18:36:17 +0530

    all

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const {userRouter} = require('./routers/userRouter');
 const {ipRouter} = require('./routers/ipRouter');
 const {ipmiddleware} = require('./middleware/ipmiddleware')
 const cors = require('cors');
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 if (typeof global.RequestCnt === 'undefined') {
   global.RequestCnt = 0;
@@ -23,11 +23,18 @@ app.use(ipmiddleware);
 app.use('/api/user',userRouter);
 app.use('/api/ip', ipRouter);
 
+app.use((error,req,res,next)=>{
+    res.status(400).json({
+        message : error
+    });
+})
+
 app.use((req,res,next)=>{
     res.status(400).json({
         message : "404 not found"
     });
 })
+
 
 app.listen(PORT,()=>{
     console.log("server is listen on port "+PORT);
